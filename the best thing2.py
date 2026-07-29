f12793b5f5c65a93e7527828d852d5b7f942ae84
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-29 11:35:50 -0700

    Update the best thing2.py

--- the best thing2.py
+++ the best thing2.py
@@ -52,7 +52,7 @@ if st.button("تأكيد التخمين "):
     st.rerun()
 if "feed" == " correct":
   st.success("اجابتك صحيحه انك اسكوره ")
-if feed == "false":
+if "feed" == "false":
   st.error("للاسف اجابتك خاطاه ")
 if st.session_state.num > 0 and st.session_state.num % 10 == 0:
   st.success("انت بطل! تحدي صديقك انه بالطبع لن يستطيع ان يصل لمستواك  ")
