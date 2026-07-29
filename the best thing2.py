18de20c66ad5884c9cf32eadf14242ac43d725f0
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-29 11:36:59 -0700

    Update the best thing2.py

--- the best thing2.py
+++ the best thing2.py
@@ -18,7 +18,7 @@ if 'num2' not in st.session_state:
 if "feed " not in st.session_state:
   st.session_state.feed=0
 if "correct" not in st.session_state:
-  st.session_state.correct=sc
+  st.session_state.correct=0
 num1=st.session_state.num1
 num2=st.session_state.num2
 sign=st.session_state.sign
