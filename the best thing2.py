ec2185e4a927f5638942961531b67144e9e24c01
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-29 11:49:02 -0700

    Update the best thing2.py

--- the best thing2.py
+++ the best thing2.py
@@ -46,13 +46,13 @@ if st.session_state.feed == " correct":
   del st.session_state.num2
   del st.session_state.sign
   st.rerun()
-if st.session_state.feed == "false":
-  st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
-  st.session_state.num =0
-  del st.session_state.num1
-  del st.session_state.num2
-  del st.session_state.sign
-  st.rerun()
+   else:
+   st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
+   st.session_state.num =0
+   del st.session_state.num1
+   del st.session_state.num2
+   del st.session_state.sign
+   st.rerun()
 if st.session_state.num > 0 and st.session_state.num % 10 == 0:
   st.success("انت بطل! تحدي صديقك انه بالطبع لن يستطيع ان يصل لمستواك  ")
   if st.button("الليفل التالي "):
